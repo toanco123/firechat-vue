d414be9c0691b31668df774ed8aeaba4c301c5b5
Viet Toan <toanco.tn@gmail.com>
2023-03-07 09:30:33 +0700

    first commit

---
+++ src/db.js
@@ -0,0 +1,13 @@
+// import * as firebase from "firebase/compat/app";
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+
+const db = firebase.initializeApp({
+  apiKey: "AIzaSyBlsj484PP9unIp5eG7faK2bMwiBpklqW4",
+  authDomain: "firechatvue-c7479.firebaseapp.com",
+  projectId: "firechatvue-c7479",
+  storageBucket: "firechatvue-c7479.appspot.com",
+  messagingSenderId: "950449546947",
+  appId: "1:950449546947:web:3b6cf92fefef27c49ba476",
+});
+export default db;
